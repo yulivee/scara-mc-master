ae607b84676713c1120bfc6a74a606a5a0f4f1bd
yulivee <coding@voiding-warranties.de>
2018-10-20 18:09:55 +0200

    Adjusted header file and added functions

--- lib/comm/comm.h
+++ lib/comm/comm.h
@@ -1,9 +1,9 @@
 #ifndef COMM_H_INCLUDED
 #define COMM_H_INCLUDED
 
-void test();
+void test(HardwareSerial &S1, HardwareSerial &S0, int *result);
 
-int ping_slave();
+int ping_slave(HardwareSerial &Serial, int slave, int message, int *error_handler);
 int drive_dist();
 int drive_to();
 int drive_dist_max();
