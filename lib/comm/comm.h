bab7b657d58ce5ecad71c8076791d9ee5d7457ce
yulivee <coding@voiding-warranties.de>
2018-10-20 17:56:55 +0200

    fixed remaining bugs from library

--- lib/comm/comm.h
+++ lib/comm/comm.h
@@ -1,10 +1,6 @@
 #ifndef COMM_H_INCLUDED
 #define COMM_H_INCLUDED
 
-#ifndef __cplusplus
-extern "C" {
-#endif
-
 void test();
 
 int ping_slave();
@@ -14,10 +10,5 @@ int drive_dist_max();
 int home();
 int set_pid_state();
 int get_position();
-
-
-#ifndef __cplusplus
-}
-#endif
 
 #endif
