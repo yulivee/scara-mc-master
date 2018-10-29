b85ef1ac94718cb37d8c866fd5766fcb3e4be4df
yulivee <coding@voiding-warranties.de>
2018-10-29 21:56:34 +0100

    implemented ros stuff

---
+++ lib/rosbridge/rosbridge.h
@@ -0,0 +1,14 @@
+#ifndef ROSBRIDGE_H_INCLUDED
+#define ROSBRIDGE_H_INCLUDED
+
+#include <Arduino.h>
+
+#define POSITION_TOPIC "GetPos"
+#define CLICK_TOPIC "WheelEncoderClicks"
+#define DRIVE_TO_TOPIC "DriveTo"
+#define DRIVE_DIST_TOPIC "DriveDist"
+#define HOME_TOPIC "Home"
+#define PID_STATE_TOPIC "SetPidState"
+#define DEBUG_TOPIC "Debug"
+
+#endif
