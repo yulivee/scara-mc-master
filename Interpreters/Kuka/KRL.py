0f963bf504dd677aa47a083938c9f8da50b30b28
Daniel Veeckman <dav.veeckman@gmail.com>
2022-12-05 23:26:48 +0100

    Added KRL Test Library

---
+++ Interpreters/Kuka/KRL.py
@@ -0,0 +1,78 @@
+#"TRIGGER WHEN DISTANCE"
+def triggerWhenDistance():
+    pass
+
+#"$BWDSTART"
+def setBwdstart():
+    pass
+
+#"PDAT_ACT"
+def setPdatAct():
+    pass
+
+#"FDAT_ACT"
+def setFdatAct():
+    pass
+
+#"BAS"
+def setBase():
+    pass
+
+#"SET_CD_PARAMS"
+def setCdParams():
+    pass
+
+#"PTP"
+def movePtp():
+    pass
+
+#"CONTINUE"
+def continueOp():
+    pass
+
+#"WHILE"
+def whileOp():
+    pass
+#"SWITCH"
+def switchOp():
+    pass
+
+#"CASE"
+def caseOp():
+    pass
+
+#"DEFAULT"
+def defaultOp():
+    pass
+
+#"LOOP"
+def loopOp():
+    pass
+
+#"HALT"
+def halt():
+    pass
+
+#"ENDLOOP"
+def endloopOp():
+    pass
+
+#"ENDSWITCH"
+def endswitchOp():
+    pass
+
+#"ENDWHILE"
+def endwhileOp():
+    pass
+
+#"END"
+def funcEnd():
+    pass
+
+#"GLOBAL DEF"
+def funcStartGlobal():
+    pass
+
+#"DEF"
+def funcStart():
+    pass
--- Interpreters/Kuka/Test.py
+++ Interpreters/Kuka/Test.py
@@ -1,43 +1,85 @@
-#------------------------------------
-#Define Variables
-#------------------------------------
-processComments=False
+
+import KRL
 
 #------------------------------------
-#Define Functions
+# Define Variables
+#------------------------------------
+bShowComments=False
+path=r"E:\Desktop\Daniel\Programming\GitRepository\Scara-Master\scara-mc-master\Interpreters\Kuka\UP_Service.src"
+dicOperators={
+    #dictionary is weighted, so specialised command words should be at the top of the keylist (eg "default" above "def")
+    "TRIGGER WHEN DISTANCE":"triggerWhenDistance", #trigger distance to point
+    "$BWDSTART":"setBwdstart",
+    "PDAT_ACT":"setPdatAct",
+    "FDAT_ACT":"setFdatAct",
+    "BAS":"setBase",
+    "SET_CD_PARAMS":"setCdParams",
+    "PTP":"movePtp",
+    "CONTINUE":"continueOp",
+    "WHILE":"whileOp",
+    "SWITCH":"switchOp",
+    "CASE":"caseOp",
+    "DEFAULT":"defaultOp",
+    "LOOP":"loopOp",
+    "HALT":"halt",
+    "ENDLOOP":"endloopOp",
+    "ENDSWITCH":"endswitchOp",
+    "ENDWHILE":"endwhileOp",
+    "END":"funcEnd",
+    "GLOBAL DEF":"funcStartGlobal",
+    "DEF":"funcStart"
+}
+keyOperators=dicOperators.keys()
+#------------------------------------
+# Define Functions
 #------------------------------------
 
-def ID_KRL(codeline):
-    print("Code: " + codeline)
+def ID_KRL(code):
+    #print("Code: " + code)
+    #split codeblock into operators,parameters and variables
+    for key in keyOperators:
+        if code.startswith(key):
+            print("Command: " + key)
 
+            #Get keywords and varables by removing the operator from "code" string
+            params=code[len(key):].strip()
+            print("Parameters: " + params)
+
+            #call function from KRL library
+            krlFunc = getattr(KRL, dicOperators[key])
+            krlFunc()
+            break
+    else: #if for loop was not exited with break function
+        print("Unknown command: " + code)
+        
 #------------------------------------
 # Main Code
 #------------------------------------
-# Open File
-path=r"E:\Desktop\Daniel\Programming\GitRepository\Scara-Master\scara-mc-master\Interpreters\Kuka\UP_Service.src"
+#open File
 path=path.replace("\\","/")
 print("Opening File:" + path)
 file = open(path)
 
-#line Number counter
-lineNr=0
 #go trough file line by line
-for line in file:
-    lineNr+=1
+for numLine, line in enumerate(file):
+
     #skip empty lines
     if line.isspace()==False:
+    
         #extract comments by splitting at the first ";" symbol
-        text = line.split(";",1)
-        #print line (if not all spaces)
-        text[0]+=" " #add a space to a potentially empty string to check empty and zero strings
-        if text[0].isspace()==False:
+        line = line.split(";",1)
+
+        #check for lines with no code
+        line[0]+=" " #add a space to a potentially empty string to check empty and zero strings
+        if line[0].isspace()==False:
             #print line number
-            print("Line: " + str(lineNr))
+            print("Line: " + str(numLine))
             #Analze Code
-            ID_KRL(text[0].strip())
-        #print comment if exists
-        if len(text)!=1 and processComments:
-            #line contained a comment      
-            print("Comment: " + text[1].strip())
+            ID_KRL(line[0].strip())
+
+        #display comment (optional)
+        if len(line)!=1 and bShowComments:      
+            print("Comment: " + line[1].strip())
+
 #close file
 file.close()
